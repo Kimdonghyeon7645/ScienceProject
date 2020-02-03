f48a994ff2b44b0bda4d02b8f2b90aa751dd947e
김동현(Donghyeon Kim) <48408417+Kimdonghyeon7645@users.noreply.github.com>
2020-02-03 23:49:18 +0900

    Update settings.py
    데이터베이스의 호스트 코드를 수정

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
          'NAME': 'pj_db',
          'USER': 'root',
          'PASSWORD': 'kkddhh77887788@',
-         'HOST': '127.0.0.1',
+         'HOST': '0.0.0.0',
          'PORT': '3306',
 	}
 }
