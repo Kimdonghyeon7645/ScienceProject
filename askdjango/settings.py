896f188c8bcad1d03f1ec4d36430f72480a14109
김동현(Donghyeon Kim) <48408417+Kimdonghyeon7645@users.noreply.github.com>
2020-02-04 00:22:52 +0900

    Update settings.py
    Invalid HTTP_HOST header: <your-site-name> . You may need to add <your-site-name> to ALLOWED_HOSTS. 라는 오류메세지 때문에 맨 마지막 한줄 코드 추가

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -130,3 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
