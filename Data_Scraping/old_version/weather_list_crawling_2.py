101b336927fbac27f77410498cc67ebc9dd68d64
김동현(Donghyeon Kim) <48408417+Kimdonghyeon7645@users.noreply.github.com>
2020-02-07 11:27:57 +0900

    Create weather_list_crawling_2.py
    weather_list_crawling_1 에서 미세먼지와 초미세먼지같은 대기환경 상태도 파악하고 데이터 긁어오기
    https://www.airkorea.or.kr/web/dustForecast?pMENU_NO=113 
    의 링크 활용

---
+++ Data_Scraping/old_version/weather_list_crawling_2.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+
+url = 'http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=3020055000'
+html = requests.get(url)
+soup = BeautifulSoup(html.text, 'html.parser')
+contents = soup.select('data')
+for content in contents:
+    hour = content.find('hour').text
+    # 현재 시간 (3시간 간격, ex> 3, 6, 9, ...)
+    today_plus_numder = content.find('day').text
+    # 오늘(현재 날짜)로 부터 몇 일뒤(+) 인지 날짜 값 (오늘이면 0일뒤므로 = 0)
+    temp = content.find('temp').text
+    # 현재 온도 (섭씨 ℃)
+    day_high_temp = content.find('tmx').text
+    # 현재 날짜의 최고 온도 (오늘은 구하지 않음, -999)
+    day_low_temp = content.find('tmn').text
+    # 현재 날짜의 최저 온도 (오늘은 구하지 않음, -999)
+    sky_state = content.find('sky').text
+    # 현재 하늘의 상태, 구름의 정도 1~4로 표현(1:맑음, 2:구름적음, 3:구름많음, 4:흐림)
+    weather_state = content.find('pty').text
+    # 현재 기상현상 상태, 0: 맑음, 1: 비, 2: 진눈개비, 3: 눈, 4: 소나기
+    weather_korean = content.find('wfkor').text
+    # 현재 기상현상 상태 통틀어서 한글로
+    rain_persent = content.find('pop').text
+    # 현재 비올 확률 (백분위, %)
+    expect_rain_6h = content.find('r06').text
+    # 6시간 뒤까지의 예상 강수량
+    expect_rain_12h = content.find('r12').text
+    # 12시간 뒤까지의 예상 강수량
+    expect_snow_6h = content.find('s06').text
+    # 6시간 뒤까지의 예상 적설량
+    expect_snow_12h = content.find('s12').text
+    # 12시간 뒤까지의 예상 적설량
+    wind_speed = content.find('ws').text
+    # 현재 풍속
+    wind_way = content.find('wd').text
+    # 현재 풍향(8분위, ex> 0: 북, 2: 동, 4: 남, 6: 서)
+    wind_korean = content.find('wden').text
+    # 현재 풍향 한글로
+    humi = content.find('reh').text
+    # 현재 습도 (백분위, %)
+
+    print("\n현재 시간: ", hour,
+          "\n오늘로 부터 몇일 뒤: ", today_plus_numder,
+          "\n현재 온도: ", temp,
+          "\n최고 온도: ", day_high_temp,
+          "\n최저 온도: ", day_low_temp,
+          "\n하늘 상황: ", sky_state,
+          "\n기상 상황: ", weather_state,
+          "\n기상 상황 한글: ", weather_korean,
+          "\n예상 강수 확률: ", rain_persent,
+          "\n예상 강수량(6시간): ", expect_rain_6h,
+          "\n예상 강수량(12시간): ", expect_rain_12h,
+          "\n예상 적설량(6시간): ", expect_snow_6h,
+          "\n예상 적설량(12시간): ", expect_rain_12h,
+          "\n풍속: ", wind_speed,
+          "\n풍향: ", wind_way,
+          "\n풍향 한글: ", wind_korean,
+          "\n습도: ", humi
+          )
+
+pms = []
+url2 = 'https://www.airkorea.or.kr/web/dustForecast?pMENU_NO=113'
+html2 = requests.get(url2)
+soup2 = BeautifulSoup(html2.text, 'html.parser')
+contents2 = soup2.select("dl.forecast.MgT50")
+for content2 in contents2:
+    print(content2.find("dt").text)
+    cols = content2.select("table.st_2 tbody tr")
+    big_pm = cols[1].select("td")[6].text
+    # 미세먼지
+    small_pm = cols[2].select("td")[6].text
+    # 초미세먼지
+    pm = [big_pm, small_pm]
+    pms.append(pm)
+
+    print(
+        "\n미세먼지: ", big_pm,
+        "\n초미세먼지: ", small_pm
+    )
