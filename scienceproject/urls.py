60949c0c8fe7d9e6695e047cafc06867009094df
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-04-29 18:31:44 +0900

    refector : hard coding...

--- scienceproject/urls.py
+++ scienceproject/urls.py
@@ -7,8 +7,12 @@ from django.conf.urls.static import static
 #app_name = "scienceproject"
 
 urlpatterns = [
-	url(r'^$', views.science_project.as_view()),
-	url('form-test/', views.test_from)
+	url(r'^$', views.gu0.as_view()),
+	url(r'0', views.gu0.as_view()),
+	url(r'1', views.gu1.as_view()),
+	url(r'2', views.gu2.as_view()),
+	url(r'3', views.gu3.as_view()),
+	url(r'4', views.gu4.as_view()),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- scienceproject/views.py
+++ scienceproject/views.py
@@ -13,34 +13,42 @@ from django.views import View
 from django.views import generic
 
 
-
-
-class science_project(generic.TemplateView):
+class gu0(generic.TemplateView):
     def get(self, request, *args, **kwargs):
         template_name = 'scienceproject/index.html'
         weather_header = Header.objects.all()
-
-        gu = request.GET.get('gu', '')
-        
-        if gu == 0:
-            weather_gu = Gu0.objects.all()
-        elif gu == 1:
-            weather_gu = Gu1.objects.all()
-        elif gu == 2:
-            weather_gu = Gu2.objects.all()
-        elif gu == 3:
-            weather_gu = Gu3.objects.all()
-        elif gu == 4:
-            weather_gu = Gu4.objects.all()
-        else:
-            weather_gu = Gu0.objects.all()
+        weather_gu = Gu0.objects.all()
 
         return render(request, template_name, {"header": weather_header, "gu0": weather_gu})
 
+class gu1(generic.TemplateView):
+    def get(self, request, *args, **kwargs):
+        template_name = 'scienceproject/index.html'
+        weather_header = Header.objects.all()
+        weather_gu = Gu1.objects.all()
 
-def test_from(request):
-    if request.method == 'POST':
-        pass
-    elif request.method == 'GET':
-        form = GuForm()
-        return render(request, 'scienceproject/index.html', {'form':form})
+        return render(request, template_name, {"header": weather_header, "gu0": weather_gu})
+
+class gu2(generic.TemplateView):
+    def get(self, request, *args, **kwargs):
+        template_name = 'scienceproject/index.html'
+        weather_header = Header.objects.all()
+        weather_gu = Gu2.objects.all()
+
+        return render(request, template_name, {"header": weather_header, "gu0": weather_gu})
+
+class gu3(generic.TemplateView):
+    def get(self, request, *args, **kwargs):
+        template_name = 'scienceproject/index.html'
+        weather_header = Header.objects.all()
+        weather_gu = Gu3.objects.all()
+
+        return render(request, template_name, {"header": weather_header, "gu0": weather_gu})
+
+class gu4(generic.TemplateView):
+    def get(self, request, *args, **kwargs):
+        template_name = 'scienceproject/index.html'
+        weather_header = Header.objects.all()
+        weather_gu = Gu4.objects.all()
+
+        return render(request, template_name, {"header": weather_header, "gu0": weather_gu})
